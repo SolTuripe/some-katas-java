61afefe67e16861f8e448d53da78d301f080cca5
Sol Turipe <solcis.bio@gmail.com>
2022-04-28 16:12:55 +0200

    setUp

--- src/test/java/morekatas/morekatas/BowlingKataTest/BowlingGameTest.java
+++ src/test/java/morekatas/morekatas/BowlingKataTest/BowlingGameTest.java
@@ -1,13 +1,21 @@
 package morekatas.morekatas.BowlingKataTest;
 
 import morekatas.morekatas.BowlingKata.BowlingGame;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
+    private BowlingGame bowlingGame;
+
+    @Before
+    void setUp() {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
-    void TestGutterGame() throws Exception {
+    void TestGutterGame() {
         BowlingGame bowlingGame = new BowlingGame();
 
         for (int i = 0; i < 20; i++) {
@@ -18,7 +26,7 @@ public class BowlingGameTest {
     }
 
     @Test
-    void TestAllOnes() throws Exception {
+    void TestAllOnes() {
         BowlingGame bowlingGame = new BowlingGame();
 
         for (int i = 0; i < 20; i++) {
