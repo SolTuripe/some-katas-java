80fce6a2a817a7b68b75f204fa32b32a9aa2cfde
Sol Turipe <solcis.bio@gmail.com>
2022-05-02 13:08:21 +0200

    add beforeEach to fizzbuzz test

--- src/test/java/morekatas/morekatas/FizzBuzzKataTest/FizzBuzzTest.java
+++ src/test/java/morekatas/morekatas/FizzBuzzKataTest/FizzBuzzTest.java
@@ -1,6 +1,7 @@
 package morekatas.morekatas.FizzBuzzKataTest;
 
 import morekatas.morekatas.FizzBuzzKata.FizzBuzz;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -8,15 +9,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fizzbuzz;
+
+    @BeforeEach
+    void setup() {
+        fizzbuzz = new FizzBuzz();
+    }
+
     @Test
     void CanStartFizzBuzz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
         assertNotNull(fizzbuzz);
     }
 
     @Test
     void NumDivBy3RetFizz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
         String expected = "Fizz";
         String actual = fizzbuzz.convert(3);
         assertEquals(expected, actual);
