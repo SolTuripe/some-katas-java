91a1587277c581f671a49f546e629008f391bc27
Sol Turipe <solcis.bio@gmail.com>
2022-05-02 13:37:55 +0200

    fizzbuzz kata finiched

--- src/main/java/morekatas/morekatas/FizzBuzzKata/FizzBuzz.java
+++ src/main/java/morekatas/morekatas/FizzBuzzKata/FizzBuzz.java
@@ -3,18 +3,16 @@ package morekatas.morekatas.FizzBuzzKata;
 public class FizzBuzz {
 
     public String convert(int i) {
-        //StringBuffer sb = new StringBuffer();
 
-        if (i % 3 == 0 && i % 5 == 0) {
+        if (i % 3 == 0 && i % 5 == 0)
             return "FizzBuzz";
-        }
 
-        if (i % 3 == 0) {
+        if (i % 3 == 0)
             return "Fizz";
-        }
-        if (i % 5 == 0) {
+
+        if (i % 5 == 0)
             return "Buzz";
-        }
+
         return Integer.toString(i);
     }
 }
