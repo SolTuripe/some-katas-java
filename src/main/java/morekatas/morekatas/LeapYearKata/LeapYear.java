f0f0dfe28392c81421140d187f6e6e8e7cc18233
Sol Turipe <solcis.bio@gmail.com>
2022-05-06 18:20:08 +0200

    I refactored the leapyear class

--- src/main/java/morekatas/morekatas/LeapYearKata/LeapYear.java
+++ src/main/java/morekatas/morekatas/LeapYearKata/LeapYear.java
@@ -1,17 +1,8 @@
 package morekatas.morekatas.LeapYearKata;
 
 public class LeapYear {
-
-    private int year;
-
-    public LeapYear(int year) {
-        this.year = year;
-    }
-
-    public boolean check(){
-        if (year % 4 == 0) {
-            return true;
-        }
-        return false;
+    boolean isLeapYear(int year) {
+        if (year % 100 == 0 && year % 400 != 0) return false;
+         return year % 400 == 0 || year % 4 == 0;
     }
 }
